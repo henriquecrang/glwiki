e1cb1f98124481da174c8aba4457cefe8a2af13a
Alberto Souza <alberto.souza.99@gmail.com>
2014-06-30 08:39:50 -0300

    fazendo alguma alterações para permitir uma maior configuração, controle do inicio do mecanismo e evitando usar o contexto global.

--- glwiki.js
+++ glwiki.js
@@ -4,57 +4,163 @@
 
 //var script = document.createElement('script');script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';script.type = 'text/javascript';document.getElementsByTagName('head')[0].appendChild(script);
 
-var enderecoLista = "User:HAndrade (WMF)/Testes/DAB"; //edite essa linha com o nome da página wiki utilizada pelo projeto para listas as palavras. Caso seu projeto ainda não tenha criado uma página acesse XXX para mais informações.
-var chamadaAPI = "http://pt.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&callback=?&titles="+enderecoLista;
+(function($){
 
-function criaLinks(palavrasChave, palavrasLink, blocosConteudo){
- var blocos = blocosConteudo.length;
- palavras = palavrasChave.length;
- for(var x = 0; x < blocos; x++){
-  texto = document.getElementById(blocosConteudo[x]).innerHTML;
-  for(var k = 0; k < palavras; k++){
-   texto = texto.replace(new RegExp(palavrasLink[k],"g"), "<a href='http://pt.wikipedia.org/wiki/"+palavrasChave[k]+"'>"+palavrasLink[k]+"</a>");
-  } // fecha for conteudo
-  document.getElementById(blocosConteudo[x]).innerHTML = texto;
- } // fecha for blocos
-} // fecha funcao criaLinks()
+  var glwiki = {};
 
+  //-- Variaveis configuraveis ---
 
-$(document).ready(function(){
-    $.ajax({
-        type: "GET",
-        url: chamadaAPI,
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-		var pageId = Object.keys(data.query.pages);
-		var texto = data.query.pages[pageId].revisions[0]["*"];
-		texto = texto.replace(/(\r\n|\n|\r)/gm,"");
-		var palavras = texto.match(/==\sPalavras.*==\sBlocos\sde\sConteúdo/g);
-		palavras = palavras[0].match(/\[\[.*?\]\]/g);
-		for(i=0;i<palavras.length;i++){ palavras[i] = palavras[i].replace(new RegExp("\\[\\[","g"), ""); }
-		for(i=0;i<palavras.length;i++){ palavras[i] = palavras[i].replace(new RegExp("\\]\\]","g"), ""); }
-		var palavrasLink = new Array();
-		for(i=0;i<palavras.length;i++){ 
-			if (palavras[i].match(/\|/)){
-				palavrasLink[i] = palavras[i].replace(new RegExp("^.*?\\|"), "");
-				palavras[i] = palavras[i].replace(new RegExp("\\|.*?$"), "");
+  // flag para iniciar automaticamente
+  glwiki.inicioAutomatico = true;
 
-			}else{
-				palavrasLink[i] = palavras[i];
-			}
-		}
+  // jquery string para selecionar os blocos de texto dentro do html da página
+  // OBS não use a tab body se o script estiver dentro dela!
+  glwiki.seletorTags = ['.node'];
 
-		var nomeBlocos = texto.match(/==\sBlocos\sde\sConteúdo.*\[\[Categoria/g);
-		nomeBlocos = nomeBlocos[0].match(/".*?"/g);
-		for(i=0;i<nomeBlocos.length;i++){ nomeBlocos[i] = nomeBlocos[i].replace(new RegExp('"','g'), ''); }
+  // lista de tags permitidas
+  glwiki.tagsPermitidas = ['div', 'p', 'span'];
+
+  glwiki.linkClass = 'glwiki';
+
+  // página padrão para pegar a lista de termos
+  glwiki.enderecoLista = "User:HAndrade (WMF)/Testes/DAB";
+
+  glwiki.chamadaAPI = "http://pt.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&callback=?&titles=";
 
 
-		criaLinks(palavras,palavrasLink,nomeBlocos);
-        },
-        error: function (errorMessage) {
-		console.log(errorMessage);
+  //-- Mecanismo ---
+
+  glwiki.criaLinks = function criaLinks(palavrasChave, palavrasLink){
+    var totalDeSeletores = glwiki.seletorTags.length;
+
+    palavras = palavrasChave.length;
+
+    for(var x = 0; x < totalDeSeletores; x++){
+      // pega as tags com o seletor
+      var tags = $(glwiki.seletorTags[x]);
+
+      // se não tem tag então pula para o proxímo seletor
+      if(!tags) continue;
+
+      // para cada tag encontrada ...
+      tags.each(function(){
+        var tag = $(this);
+        // pega o nome da tag para checar se está na lista de tags permitidas
+        var tagName = tag.prop("tagName");
+
+        // só adiciona o link para as tags permitidas
+        if(glwiki.tagPermitida(tagName)){
+          //busca pelos termos dentro do html
+          texto = tag.html();
+          for(var k = 0; k < palavras; k++){
+            // formata a palavra chave transformando em link
+            var tagLink = glwiki.formatarTagDoLink(palavrasChave[k], palavrasLink[k]);
+            // troca o texto encontrado pela tag
+            texto = texto.replace(new RegExp(palavrasLink[k],"g"), tagLink);
+          } // fecha for conteudo
+
+          tag.html(texto);
         }
+      });
+
+    } // fecha for blocos
+  };
+
+  /**
+   * Formata uma tag de link com a palavra chave e texto do link informados
+   * @param  {string} palavraChave  palavra chave
+   * @param  {string} texto   texto para adicionar dentro do link
+   * @return {string}              texto da tag link
+   */
+  glwiki.formatarTagDoLink = function formatarTagDoLink(palavraChave, texto){
+    var link = "http://pt.wikipedia.org/wiki/"+palavraChave;
+    return "<a class='"+glwiki.linkClass+"' href='"+link+"'>"+texto+"</a>";
+  };
+
+  /**
+   * Pega a lista de palavras de uma página na wikipedia
+   * ao terminar executa o callback passando uma lista de palavras
+   * @param {string callback   callback(palavras);
+   * @return {void}
+   */
+  glwiki.getPalavras = function getPalavras(callback){
+    $.ajax({
+      type: "GET",
+      url: glwiki.chamadaAPI + glwiki.enderecoLista,
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      success: function (data, textStatus, jqXHR) {
+        var pageId = Object.keys(data.query.pages);
+        texto = data.query.pages[pageId].revisions[0]["*"];
+        texto = texto.replace(/(\r\n|\n|\r)/gm,"");
+        var palavras = texto.match(/==\sPalavras.*==\sBlocos\sde\sConteúdo/g);
+        palavras = palavras[0].match(/\[\[.*?\]\]/g);
+        for(i=0;i<palavras.length;i++){
+          palavras[i] = palavras[i].replace(new RegExp("\\[\\[","g"), "");
+        }
+        for(i=0;i<palavras.length;i++){
+          palavras[i] = palavras[i].replace(new RegExp("\\]\\]","g"), "");
+        }
+
+        // salva as palavras no glwiki
+        glwiki.palavras = palavras;
+        // roda o callback passando as palavras
+        callback(palavras);
+      },
+      error: function (errorMessage) {
+        console.error(errorMessage);
+      }
     });
-});
+  };
+
+  // flag para marcar se o glwiki iniciou
+  glwiki.iniciou = false;
+  /**
+   * Função para iniciar o script
+   */
+  glwiki.init = function init(){
+    // só permite que o glwiki inicie 1 vez
+    if(glwiki.iniciou) return;
+    glwiki.iniciou = true;
+
+    // carrega as palavas da wiki
+    glwiki.getPalavras(function(palavras){
+      var palavrasLink = new Array();
+      for(i=0;i<palavras.length;i++){
+        if (palavras[i].match(/\|/)){
+          palavrasLink[i] = palavras[i].replace(new RegExp("^.*?\\|"), "");
+          palavras[i] = palavras[i].replace(new RegExp("\\|.*?$"), "");
+        }else{
+          palavrasLink[i] = palavras[i];
+        }
+      }
+
+      glwiki.criaLinks(palavras,palavrasLink);
+
+    });
+  };
+
+
+  // ---- HELPERS ---- //
+  /**
+   * Verifica se a tag é permitida - se está na lista de tags permitidas
+   * @param  {string}  tag
+   * @return {boolean}  retorna verdadeiro ou falso
+   */
+  glwiki.tagPermitida = function tagPermitida(tag){
+    return $.inArray(tag.toLowerCase(), glwiki.tagsPermitidas) > -1;
+  };
+
+
+  $(document).ready(function(){
+    // se inicioAutomatico estiver ativado inicia o script marcando as tags
+    if(glwiki.inicioAutomatico){
+      glwiki.init();
+    }
+  });
+
+  // exportando para o contexto global do navegador
+  window.glwiki = glwiki;
+
+})(jQuery);
